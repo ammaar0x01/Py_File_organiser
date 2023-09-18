b29ec983d5d6d6f1179839860bf5503456c9c316
trpl-A <amr.swartland@gmail.com>
2023-09-19 00:15:38 +0200

    created calc

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # program:      Calculator
-- desc:         A calculator using a python dictionary
+- desc:         A calculator using a python dictionary; 
+                operations with 2 numbers
 - programmer:   trpl-A
 - started:      18.09.23
 - updated:      18.09.23
-- status:       work-in-progress
+- status:       COMPLETE; connected to github repo
 
 More notes
 ==========
-
+calc.py         -> the best response to the problem; the best solution
 <end>
--- calc.py
+++ calc.py
@@ -1,10 +1,28 @@
-#
-# ==================
+import math 
 
-# functions
+# FUNCTIONS
 # non-void
 def add(a, b) -> int:
     return a + b 
+
+def subtract(a, b) -> int:
+    return a - b 
+
+def multiply(a, b) -> int:
+    return a * b 
+
+def divide(a, b) -> int:
+    return a / b 
+
+
+def modulus(a, b) -> int:
+    return a % b 
+
+def pow(a, b) -> int:
+    return a ** b 
+
+def sqrt(a, b) -> int:
+    return math.sqrt(a, b)
 
 
 # void
@@ -13,30 +31,46 @@ def banner() -> None:
     print("2. -")
     print("3. *")
     print("4. /")
+    print("5. %")
+    print("6. **")
+    print("7. sqrt")
 
-def check_operation(op) -> None:
-    if op == "+":
-        add()
+def get_nums():
+    correct_input = False
 
-# ==================
+    while not correct_input:
+        num1 = input("Enter a number: ")
+        num2 = input("Enter another number: ")
+         
+        try: 
+            num1 = int(num1)
+            num2 = int(num2)
+            correct_input = True
+
+        except:
+                print("*an error has occurred*")
+                print("Enter type *int*\n")
+
+    return [num1, num2] 
 
 
+# DICTIONARY
 dict_calc = {
-
+    "+": add,
+    "-": subtract,
+    "*": multiply, 
+    "/": divide,
+    "%": modulus,
+    "**": pow,
+    "sqrt": sqrt 
 }
-# ==================
 
+def check_operation(op, n1, n2) -> None:
+    try:
+        print(dict_calc[op](n1, n2))   
 
-# user input
-num1 = input("Enter a number: ")
-num2 = input("Enter another number")
-
-operation = input("Select an operation: ")
-
-# ===============================
-def main():
-    print("main")
-
-if __name__ == "__main__":
-    print("\n<END OF PROGRAM>")
-# ===============================
+    except:
+        print("*an error has occurred*")
+        print("operation may not exist in dictionary")
+# ======================================
+# <END>
